18da3fc77a86b34dde273ab852323985259bb745
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-09 14:36:31 -0400

    Initial solution commit.

--- src/main/java/com/valuablecode/kata/FizzBuzz.java
+++ src/main/java/com/valuablecode/kata/FizzBuzz.java
@@ -2,4 +2,17 @@ package com.valuablecode.kata;
 
 public class FizzBuzz {
 
+    public String classify(int n) {
+        if (n % 3 == 0 && n % 5 == 0)
+            return "fizzbuzz";
+
+        if (n % 5 == 0)
+            return "buzz";
+
+        if (n % 3 == 0)
+            return "fizz";
+
+        return Integer.toString(n);
+    }
+
 }
--- src/test/java/com/valuablecode/kata/FizzBuzzTest.java
+++ src/test/java/com/valuablecode/kata/FizzBuzzTest.java
@@ -1,13 +1,38 @@
 package com.valuablecode.kata;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+import com.valuablecode.kata.FizzBuzz;
+
 public class FizzBuzzTest {
 
+    final FizzBuzz sut = new FizzBuzz();
+
     @Test
-    public void describe_behaviour() {
-        fail();
+    public void classify_regular_numbers_as_is() {
+        assertThat(sut.classify(1), equalTo("1"));
+        assertThat(sut.classify(2), equalTo("2"));
     }
+
+    @Test
+    public void classify_divisible_by_3_as_fizz() {
+        assertThat(sut.classify(3), equalTo("fizz"));
+        assertThat(sut.classify(6), equalTo("fizz"));
+    }
+
+    @Test
+    public void classify_divisible_5_as_buzz() {
+        assertThat(sut.classify(5), equalTo("buzz"));
+        assertThat(sut.classify(10), equalTo("buzz"));
+    }
+
+    @Test
+    public void classify_divisible_by_both_3_and_5_as_fizzbuzz() {
+        assertThat(sut.classify(15), equalTo("fizzbuzz"));
+        assertThat(sut.classify(30), equalTo("fizzbuzz"));
+    }
+
 }
